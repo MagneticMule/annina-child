f4e91ebbb2d4a6915a58fe9ed386262c10a9202f
Thomas Sweeney <skywriter@gmail.com>
2015-08-14 14:08:34 +0100

    Added columns in editor view

--- functions.php
+++ functions.php
@@ -15,3 +15,29 @@ function theme_enqueue_styles()
 {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+add_editor_style( 'editor-style.css' );
+
+/**
+ * defines the visual layout to be added in to the Wordpress content object
+ * @param  [type] $content [description]
+ * @return [type]          [description]
+ */
+function custom_layout_injector( $content )
+{
+    // assemble the custom content
+    $content = '
+    <div class="content-col-main">
+        &nbsp;
+        Main content goes here.
+    </div>
+
+    <div class="content-col-right">
+        &nbsp;
+        Side content goes here.
+    </div>
+
+    ';
+    return $content;
+}
+add_filter('default_content', 'custom_layout_injector' );
