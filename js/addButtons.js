ac435c709ed3d81e8069fb7971f8b781e049c3af
Thomas Sweeney <skywriter@gmail.com>
2015-08-22 17:17:28 +0100

    This and mostly that

--- js/addButtons.js
+++
@@ -1,9 +0,0 @@
-/*jQuery(document).ready(
-    function($) {
-        console.log('ShowHide Script loaded ....');
-        jQuery( '.button-container' ).append( '<input value="Transcript" type="button" class="right" onclick="showHide(this);" />' );
-    });
-*/
-
-
-
