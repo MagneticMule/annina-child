d60d3215d37ce95c3a848fa08ff643dbde100307
Thomas Sweeney <skywriter@gmail.com>
2015-08-23 10:21:42 +0100

    This and mostly that

--- js/showHide.js
+++ js/showHide.js
@@ -3,6 +3,6 @@ jQuery(document).ready(
         console.log("Star Trekking, across the universe...");
         jQuery( '.button-container' ).append( '<input value="Transcript" type="button" id="showtranscript" style="margin: 0;"/>' );
         jQuery('#showtranscript').click(function() {
-            jQuery('.show-hide').slideToggle('medium', 'swing / linear');
+            jQuery('.show-hide').slideToggle('medium');
         });
     });
