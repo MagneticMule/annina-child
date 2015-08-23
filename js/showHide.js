68355be21dd69f7370c5a3f05a90663976429d6c
Thomas Sweeney <skywriter@gmail.com>
2015-08-23 18:20:08 +0100

    Added tabs to activities

--- js/showHide.js
+++ js/showHide.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(
     function($) {
-        jQuery('#tab-container').easytabs();
         console.log("Star Trekking, across the universe...");
         jQuery( '.button-container' ).append( '<input value="Show" type="button" id="showtranscript" style="margin: 0;"/>' );
         jQuery('#showtranscript').click(function() {
