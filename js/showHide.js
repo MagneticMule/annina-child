bf146cd7b4443c4fbe2c621c0a5a69e46859b00c
Thomas Sweeney <skywriter@gmail.com>
2015-08-23 12:44:19 +0100

    This and mostly that

--- js/showHide.js
+++ js/showHide.js
@@ -1,7 +1,8 @@
 jQuery(document).ready(
     function($) {
+        jQuery('#tab-container').easytabs();
         console.log("Star Trekking, across the universe...");
-        jQuery( '.button-container' ).append( '<input value="Transcript" type="button" id="showtranscript" style="margin: 0;"/>' );
+        jQuery( '.button-container' ).append( '<input value="Show" type="button" id="showtranscript" style="margin: 0;"/>' );
         jQuery('#showtranscript').click(function() {
             jQuery('.show-hide').slideToggle('medium');
         });
